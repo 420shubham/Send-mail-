b86fa71b6d045015cfd9e17680078bf9f76fcf74
shubham420 <021bscot038@sxc.edu.np>
2024-12-22 17:43:07 +0545

    final commit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 ##################### Extra Hard Starting Project ######################
 import pandas
+import datetime as dt
+import smtplib
+import random
+
 # 1. Update the birthdays.csv
 df = pandas.read_csv("D:/ramesh.belbase/Desktop/leaen/LEARNING PYTHON/GRAMS/DAY-32/birthdays.csv")
 date = df.to_dict(orient="records")
@@ -7,10 +11,6 @@ date = df.to_dict(orient="records")
 
 
 # 2. Check if today matches a birthday in the birthdays.csv
-import datetime as dt
-import smtplib
-import random
-
 my_email = "shubhamb42069@gmail.com"
 password = "pmif sdeb bkgi pzir"
 letter_templates = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
@@ -21,12 +21,17 @@ today_day = today.day
 
 for i in date:
     if i["month"] == today_month and i["day"] == today_day:
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
         txt = random.choice(letter_templates)
         file_path = f"D:/ramesh.belbase/Desktop/leaen/LEARNING PYTHON/GRAMS/DAY-32/letter_templates/{txt}"
         with open(file_path, "r") as file:
             content = file.read()
             message = content.replace("[NAME]", i["name"])
 
+
+
+# 4. Send the letter generated in step 3 to that person's email address.
         with smtplib.SMTP("smtp.gmail.com") as connect:
             connect.starttls()
             connect.login(user=my_email, password=password)
@@ -34,9 +39,9 @@ for i in date:
 
 
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
-# 4. Send the letter generated in step 3 to that person's email address.
+
+
 
 
 
